c8a4be3ee07ce5128ce2c2a4b678b44107796bc7
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-20 16:12:33 -0400

    Makes significant changes to auth helpers

--- api/auth/router.js
+++ api/auth/router.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
 const db = require('./model');
-const {grabOrgRoles, genToken} = require('./helpers');
+const {genToken, grabOrgRoles, formatOrgRoles} = require('./helpers');
 
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
   db.login({username})
     .then(async user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = await genToken(user);
-        const org_roles = await grabOrgRoles(user.id)
+        const grabbed = await grabOrgRoles(user.id);
+        const org_roles = formatOrgRoles(grabbed);
+        const token = await genToken(user, org_roles);
         res.status(201).json({token, org_roles});
       } else {
         res.status(401).json({
