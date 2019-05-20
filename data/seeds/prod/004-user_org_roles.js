efb545449156f252f638ac5a9d42706dd0cf7e0c
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-20 14:59:09 -0400

    Updates production seeds

--- data/seeds/prod/004-user_org_roles.js
+++ data/seeds/prod/004-user_org_roles.js
@@ -4,49 +4,30 @@ exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE TABLE user_org_roles RESTART IDENTITY CASCADE')
     .then(function () {
       return knex('user_org_roles').insert([
-        // User1
         {
           user_id: 1,
-          org_id: 1,
-          role_id: 1,
-        },
-        {
-          user_id: 1,
-          org_id: 1,
-          role_id: 2,
-        },
-        {
-          user_id: 1,
-          org_id: 2,
-          role_id: 1,
-        },
-        {
-          user_id: 1,
-          org_id: 2,
-          role_id: 2,
-        },
-        // User2
-        {
-          user_id: 2,
           org_id: 1,
           role_id: 1,
         },
         {
           user_id: 2,
-          org_id: 2,
+          org_id: 1,
           role_id: 2,
         },
-        // User3
         {
           user_id: 3,
-          org_id: 1,
+          org_id: 2,
           role_id: 1,
         },
-        // User4
         {
           user_id: 4,
           org_id: 2,
           role_id: 2,
+        },
+        {
+          user_id: 5,
+          org_id: 1,
+          role_id: 1,
         }
       ]);
     });
