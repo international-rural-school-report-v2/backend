11dc4b34b8e2dad54ef23b863cbc78977ece70f1
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-21 17:51:21 -0400

    Should fix /issues/:id PUT & DELETE

--- api/issues/router.js
+++ api/issues/router.js
@@ -41,8 +41,8 @@ router.get('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   const {id} = req.params;
   const issue = req.body;
-  const {subject} = req.decoded;
-  const {org_id} = org_roles[0].org_roles;
+  const {subject, org_roles} = req.decoded;
+  const {org_id} = org_roles[0];
   return db.putIssue(id, issue, subject, org_id)
     .then(issues => {
       res.status(200).json(issues);
@@ -54,7 +54,8 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const {id} = req.params;
-  const {org_id} = org_roles[0].org_roles;
+  const {org_roles} = req.decoded;
+  const {org_id} = org_roles[0];
   return db.delIssue(id, org_id)
     .then(issues => {
       res.status(200).json(issues);
@@ -72,20 +73,6 @@ router.get('/org/:org_id', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ error: 'Could not retrieve issues from the database' })
-    })
-})
-
-router.post('/org/:org_id', (req, res) => {
-  const {org_id} = req.params;
-  let issue = req.body;
-  issue = { ...issue, org_id };
-  const {subject} = req.decoded;
-  return db.postIssue(issue, subject)
-    .then(issues => {
-      res.status(200).json(issues);
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'Could not create the new issue' })
     })
 })
 
