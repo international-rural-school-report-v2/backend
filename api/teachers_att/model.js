bdd06ab0d2880a1be576993bbd4d5e1773a3b954
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-23 13:00:47 -0400

    Fixes typo in teachers_att model

--- api/teachers_att/model.js
+++ api/teachers_att/model.js
@@ -3,7 +3,7 @@ const db = require('../../data/dbConfig.js')
 const getTeachersAttData = () => {
   return db('teach_att')
     .select('*')
-    .orderBy('date', 'desc');;
+    .orderBy('date', 'desc');
 };
 
 module.exports = { getTeachersAttData }
